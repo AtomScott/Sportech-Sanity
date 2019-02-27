7fbf825cf9ea888d8a53c3b55618b2e9184dd659
Atom Scott <atom.james.scott@gmail.com>
2019-02-27 12:43:40 +0900

    Updated with make.sh

--- make_index.py
+++ make_index.py
@@ -14,30 +14,30 @@ for dir2 in dir1:
     lg_title = div(cls="row", id=dir2)
     lg_title.add(div(cls="col-sm-12").add(h3(dir2,cls="category_header")))
 
-    sm_block = div(cls="row")
     for dir3 in os.listdir("content/{0}".format(dir2)):
         if dir3 == ".ipynb_checkpoints":
             continue
-        card = div(cls="card")
-        card_header = h4(dir3, cls="card-header")
-        card_body = div(cls="card-body")
 
-        list = ul('\
+        block = '\
+        <div class="row">\
+        <div class="col-sm-12">\
+        <div class="card">\
+        <h4 class="card-header">{0}</h4>\
+        <div class="card-body">\
+        <ul>\
         {{ range (where .Pages "File.Dir" "in" "/soccer/").Reverse }}/\
         <li>\
         <a href="{{.Permalink}}">{{.Title}}</a>\
         </li>\
         {{ end }}\
-        ')
-        card_body.add(list)
-        card.add(card_header)
-        card.add(card_body)
-        sm_block.add(card)
+        </ul> \
+        </div>\
+        </div>\
+        </div>\
+        </div>\
+        '.format(dir3)
 
-    block = div()
-    block.add(lg_title)
-    block.add(sm_block)
+    print(lg_title, file=codecs.open(file, 'a', 'utf-8'))
     print(block, file=codecs.open(file, 'a', 'utf-8'))
-
 
 print('</div> \n</main> \n {{ end }}', file=codecs.open(file, 'a', 'utf-8'))
